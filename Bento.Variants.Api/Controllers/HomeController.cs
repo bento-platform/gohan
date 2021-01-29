cc4c87284398b426ea784873142a66b904d32fef
brouillette <brennan.brouillette@computationalgenomics.ca>
2021-01-29 14:26:07 -0500

    Initial commit - basic project structure

---
+++ Bento.Variants.Api/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Bento.Variants.Api.Models;
+
+namespace Bento.Variants.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Variant Service";
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Bento.Variants.Api/Startup.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Bento.Variants.Api.Repositories;
+using Bento.Variants.Api.Repositories.Interfaces;
+
+namespace Bento.Variants.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            // Logs + Elastic
+            services.AddElasticSearch(Configuration);
+
+            // MVC
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        
+            // -- IoC configuration --
+            ConfigureRepositoryIoC(services);
+
+        }
+
+
+        private void ConfigureRepositoryIoC(IServiceCollection services)
+        {
+            // -- Repository Configuration --
+            services.AddTransient<IElasticRepository, ElasticRepository>();
+            // -- - --
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
