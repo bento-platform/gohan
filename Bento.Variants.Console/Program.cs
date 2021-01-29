ea22d9d12d6c846237bb6dfea79de9d9fbc69245
Brennan Brouillette <brennan.brouillette.culliford@gmail.com>
2021-01-29 16:28:58 -0500

    introducing: - data volume - docker-compose - console for vcf parsing

---
+++ Bento.Variants.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FolkerKinzel.VCards;
+using Nest;
+
+namespace Bento.Variants.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            // Establish connection with local Elasticsearch
+            var url = "http://0.0.0.0:9200";
+            var indexMap = "bento-variants-data";
+
+            var settings = new ConnectionSettings(new Uri(url))
+                .DefaultIndex(indexMap);
+
+            var client = new ElasticClient(settings);
+
+            // TODO: load 1000Genomes and push to Elasticsearch
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## Prerequisites:
+
+- .NET Core 2.1
+- Elasticsearch
+  - getting started: https://www.elastic.co/guide/en/elasticsearch/reference/current/getting-started.html
+  - overview tutorial: https://www.youtube.com/watch?v=C3tlMqaNSaI
+- Docker
+  - getting started: https://www.docker.com/get-started
+
+<br />
