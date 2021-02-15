bd1d3b96448c93dc5bf0f8033e7d968dd387bb9a
brouillette <brennan.brouillette@computationalgenomics.ca>
2021-02-15 18:32:55 -0500

    parameterized include/exclude samples in GETs

--- Bento.Variants.Api/Repositories/Interfaces/IElasticRepository.cs
+++ Bento.Variants.Api/Repositories/Interfaces/IElasticRepository.cs
@@ -8,6 +8,11 @@ namespace Bento.Variants.Api.Repositories.Interfaces
     {
         Task<long> CountDocumentsContainingVariantOrSampleIdInPositionRange(double? chromosome, string variantId, string sampleId, double? lowerBound, double? upperBound);
         
-        Task<List<dynamic>> GetDocumentsContainingVariantOrSampleIdInPositionRange(double? chromosome, string variantId, string sampleId, double? lowerBound, double? upperBound, int size = 100, string sortByPosition = null);
+        Task<List<dynamic>> GetDocumentsContainingVariantOrSampleIdInPositionRange(
+            double? chromosome, 
+            string variantId, string sampleId, 
+            double? lowerBound, double? upperBound, 
+            int size = 100, string sortByPosition = null,
+            bool includeSamplesInResultSet = true);
     }
 }
