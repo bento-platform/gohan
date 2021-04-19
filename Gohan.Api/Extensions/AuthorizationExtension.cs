6f7df53145b56dfdbfd86ac3a17d0a719acdd152
brouillette <brennan.brouillette@computationalgenomics.ca>
2021-04-19 15:54:06 -0400

    better authz security configuration

--- Gohan.Api/Extensions/AuthorizationExtension.cs
+++ Gohan.Api/Extensions/AuthorizationExtension.cs
@@ -14,6 +14,7 @@ namespace Gohan.Api
         public static void AddAuthorizationLayer(this IServiceCollection services, IConfiguration configuration)
         {
             var isEnabled = $"{configuration["Authorization:IsEnabled"]}";
+            var agreedToDisabledAuthzRiskTerms = $"{configuration["Authorization:AgreedToDisabledAuthzRiskTerms"]}";
             var oidcJwksUrl = $"{configuration["Authentication:OidcPublicJwksUrl"]}";
             var opaUrl = $"{configuration["Authorization:OpaUrl"]}";
             var reqHeaders = $"{configuration["Authorization:RequiredHeadersCommaSep"]}".Split(",").ToList();
@@ -25,10 +26,14 @@ namespace Gohan.Api
             }
 
 #if DEBUG == false
-            if(Boolean.Parse(isEnabled) == false)
+            if(Boolean.Parse(isEnabled) == false && Boolean.Parse(agreedToDisabledAuthzRiskTerms) == false)
             {
-                throw new Exception($"STARTUP ERROR: Not running in development mode, but Data Access Authorization is disabled!" +
-                                     "Please enable Data Access Authorization and redeploy! -- Aborting");
+                throw new Exception($"STARTUP ERROR: Data Access Authorization is disabled and Gohan Api is not running in development mode!\n" +
+                                     "The `IsEnabled` toggle switch is intended to be used in development mode only for ease of use.\n" +
+                                     "If you know what you are doing, and this was configured intentionally, please note that all " +
+                                     "data will be exposed publically and will be availble to anyone without discrimination!\n" +
+                                     "If you agree to these terms, please set the `AgreedToDisabledAuthzRiskTerms` configuration to `true`. " +
+                                     "Otherwise, please enable Data Access Authorization by setting the `IsEnabled` configuration to `true` and redeploy! -- Aborting");
             }
 #endif
 
