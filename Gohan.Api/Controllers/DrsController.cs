88269490b426766e958a64eda93c026f1fdf880b
Brennan Brouillette <brennan.brouillette.culliford@gmail.com>
2021-04-19 18:59:59 -0400

    begin api-drs interface

---
+++ Gohan.Api/Controllers/DrsController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+using Gohan.Api.Middleware;
+using Gohan.Api.Repositories.Interfaces;
+using Gohan.Api.Services.Interfaces;
+
+using Gohan.XCC;
+
+using Newtonsoft.Json;
+
+namespace Gohan.Api.Controllers
+{
+    [Route("drs")]
+    [MandateAuthorizationTokens]
+    public class DrsController : Controller
+    {
+        private readonly IConfiguration Configuration;
+        private readonly IDrsRepository DrsRepository;
+        
+        public DrsController(
+            IDrsRepository drsRepository,
+            IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+            this.DrsRepository = drsRepository;
+        }
+
+        [HttpGet]
+        [Route("get/objects/{objectId}")]
+        public async Task<IActionResult> GetObjectsById([FromRoute] string objectId)
+        {
+            var jsonString = await DrsRepository.GetObjectById(objectId);
+            return Json(jsonString);             
+        }
+
+        [HttpGet]
+        [Route("get/objects/{objectId}/download")]
+        public async Task<IActionResult> DownloadObjectsById([FromRoute] string objectId)
+        {
+            var objectBytes = await DrsRepository.DownloadObjectById(objectId);
+            return File(objectBytes, "application/octet-stream");
+        }
+    }
+}
