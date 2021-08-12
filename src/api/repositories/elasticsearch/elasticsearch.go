61a1ea422bad1d3a279ed1b8eb5f6a5161623153
brouillette <brennan.brouillette@computationalgenomics.ca>
2021-08-12 15:22:51 -0400

    implemented variants overview endpoint

--- src/api/repositories/elasticsearch/elasticsearch.go
+++ src/api/repositories/elasticsearch/elasticsearch.go
@@ -333,3 +333,60 @@ func CountDocumentsContainerVariantOrSampleIdInPositionRange(es *elasticsearch.C
 
 	return result
 }
+
+func GetBucketsByKeyword(es *elasticsearch.Client, keyword string) map[string]interface{} {
+
+	// begin building the request body.
+	var buf bytes.Buffer
+	aggMap := map[string]interface{}{
+		"size": "0",
+		"aggs": map[string]interface{}{
+			"items": map[string]interface{}{
+				"terms": map[string]interface{}{
+					"field": keyword,
+				},
+			},
+		},
+	}
+
+	// encode the query
+	if err := json.NewEncoder(&buf).Encode(aggMap); err != nil {
+		log.Fatalf("Error encoding aggMap: %s\n", err)
+	}
+
+	// TEMP: SECURITY RISK
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	//
+	// Perform the search request.
+	res, searchErr := es.Search(
+		es.Search.WithContext(context.Background()),
+		es.Search.WithIndex("variants"),
+		es.Search.WithBody(&buf),
+		es.Search.WithTrackTotalHits(true),
+		es.Search.WithPretty(),
+	)
+	if searchErr != nil {
+		fmt.Printf("Error getting response: %s\n", searchErr)
+	}
+
+	defer res.Body.Close()
+
+	// Temp
+	resultString := res.String()
+	fmt.Println(resultString)
+	// --
+
+	// Declared an empty interface
+	result := make(map[string]interface{})
+
+	// Unmarshal or Decode the JSON to the interface.
+	// Known bug: response comes back with a preceing '[200 OK] ' which needs trimming (hence the [9:])
+	umErr := json.Unmarshal([]byte(resultString[9:]), &result)
+	if umErr != nil {
+		fmt.Printf("Error unmarshalling response: %s\n", umErr)
+	}
+
+	fmt.Printf("Query End: %s\n", time.Now())
+
+	return result
+}
