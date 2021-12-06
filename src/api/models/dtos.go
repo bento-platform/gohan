685327ae89ba89903fe29e7566cc4d57ba16f109
brouillette <brennan.brouillette@computationalgenomics.ca>
2021-12-06 18:35:32 -0500

    whoops, wrong type

--- src/api/models/dtos.go
+++ src/api/models/dtos.go
@@ -1,17 +1,17 @@
 package models
 
 type VariantsResponseDTO struct {
-	Status  int                        `json:"status"`
-	Message string                     `json:"message"`
-	Data    []VariantResponseDataModel `json:"data"`
+	Status   int                        `json:"status"`
+	Message  string                     `json:"message"`
+	Data     []VariantResponseDataModel `json:"data"`
+	DataType string                     `json:"data_type"` // i.e.: "variants"
 }
 
 type VariantResponseDataModel struct {
 	VariantId string      `json:"variantId"`
 	SampleId  string      `json:"sampleId"`
 	Count     int         `json:"count"`
-	Results   interface{} `json:"results"`   // i.e.: []Variant or []string
-	DataType  string      `json:"data_type"` // i.e.: "variants"
+	Results   interface{} `json:"results"` // i.e.: []Variant or []string
 }
 
 type GenesResponseDTO struct {
