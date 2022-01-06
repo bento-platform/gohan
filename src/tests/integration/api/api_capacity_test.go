2b161e00128080a25a3b4af8a22e3f668352e2a8
brouillette <brennan.brouillette@computationalgenomics.ca>
2022-01-06 18:24:59 -0500

    temp disabled api capacity test

--- src/tests/integration/api/api_capacity_test.go
+++ src/tests/integration/api/api_capacity_test.go
@@ -1,84 +1,84 @@
 package api
 
-import (
-	"api/models/dtos"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"regexp"
-	"sync"
-	"testing"
-	common "tests/common"
+// import (
+// 	"api/models/dtos"
+// 	"encoding/json"
+// 	"fmt"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"regexp"
+// 	"sync"
+// 	"testing"
+// 	common "tests/common"
 
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/stretchr/testify/assert"
+// )
 
-func TestCanHandleIngestAllLocalVcfs(_t *testing.T) {
-	cfg := common.InitConfig()
+// func TestCanHandleIngestAllLocalVcfs(_t *testing.T) {
+// 	cfg := common.InitConfig()
 
-	var vcfGzfiles []string
+// 	var vcfGzfiles []string
 
-	fileInfo, err := ioutil.ReadDir(cfg.Api.LocalVcfPath)
-	if err != nil {
-		fmt.Printf("Failed: %s\n", err)
-	}
+// 	fileInfo, err := ioutil.ReadDir(cfg.Api.LocalVcfPath)
+// 	if err != nil {
+// 		fmt.Printf("Failed: %s\n", err)
+// 	}
 
-	// Filter only .vcf.gz files
-	for _, file := range fileInfo {
-		if matched, _ := regexp.MatchString(".vcf.gz", file.Name()); matched {
-			vcfGzfiles = append(vcfGzfiles, file.Name())
-		} else {
-			fmt.Printf("Skipping %s\n", file.Name())
-		}
-	}
+// 	// Filter only .vcf.gz files
+// 	for _, file := range fileInfo {
+// 		if matched, _ := regexp.MatchString(".vcf.gz", file.Name()); matched {
+// 			vcfGzfiles = append(vcfGzfiles, file.Name())
+// 		} else {
+// 			fmt.Printf("Skipping %s\n", file.Name())
+// 		}
+// 	}
 
-	var combWg sync.WaitGroup
-	simultaneousRequestsCapacity := 1 // TODO: tweak
-	simultaneousRequestsQueue := make(chan bool, simultaneousRequestsCapacity)
+// 	var combWg sync.WaitGroup
+// 	simultaneousRequestsCapacity := 1 // TODO: tweak
+// 	simultaneousRequestsQueue := make(chan bool, simultaneousRequestsCapacity)
 
-	// fire and forget
-	for _, vcfgz := range vcfGzfiles {
-		simultaneousRequestsQueue <- true
-		combWg.Add(1)
-		go func(_vcfgz string, _combWg *sync.WaitGroup) {
-			defer _combWg.Done()
-			defer func() { <-simultaneousRequestsQueue }()
+// 	// fire and forget
+// 	for _, vcfgz := range vcfGzfiles {
+// 		simultaneousRequestsQueue <- true
+// 		combWg.Add(1)
+// 		go func(_vcfgz string, _combWg *sync.WaitGroup) {
+// 			defer _combWg.Done()
+// 			defer func() { <-simultaneousRequestsQueue }()
 
-			url := cfg.Api.Url + fmt.Sprintf("/variants/ingestion/run?fileNames=%s&assemblyId=GRCH37&filterOutHomozygousReferences=true", _vcfgz)
+// 			url := cfg.Api.Url + fmt.Sprintf("/variants/ingestion/run?fileNames=%s&assemblyId=GRCH37&filterOutHomozygousReferences=true", _vcfgz)
 
-			fmt.Printf("Calling %s\n", url)
-			request, _ := http.NewRequest("GET", url, nil)
+// 			fmt.Printf("Calling %s\n", url)
+// 			request, _ := http.NewRequest("GET", url, nil)
 
-			client := &http.Client{}
-			response, responseErr := client.Do(request)
-			assert.Nil(_t, responseErr)
+// 			client := &http.Client{}
+// 			response, responseErr := client.Do(request)
+// 			assert.Nil(_t, responseErr)
 
-			defer response.Body.Close()
+// 			defer response.Body.Close()
 
-			// this test (at the time of writing) will only work if authorization is disabled
-			shouldBe := 200
-			assert.Equal(_t, shouldBe, response.StatusCode, fmt.Sprintf("Error -- Api GET %s Status: %s ; Should be %d", url, response.Status, shouldBe))
+// 			// this test (at the time of writing) will only work if authorization is disabled
+// 			shouldBe := 200
+// 			assert.Equal(_t, shouldBe, response.StatusCode, fmt.Sprintf("Error -- Api GET %s Status: %s ; Should be %d", url, response.Status, shouldBe))
 
-			//	-- interpret array of ingestion requests from response
-			respBody, respBodyErr := ioutil.ReadAll(response.Body)
-			assert.Nil(_t, respBodyErr)
+// 			//	-- interpret array of ingestion requests from response
+// 			respBody, respBodyErr := ioutil.ReadAll(response.Body)
+// 			assert.Nil(_t, respBodyErr)
 
-			//	--- transform body bytes to string
-			respBodyString := string(respBody)
+// 			//	--- transform body bytes to string
+// 			respBodyString := string(respBody)
 
-			//	-- convert to json and check for error
-			var respDto dtos.VariantsResponseDTO
-			jsonUnmarshallingError := json.Unmarshal([]byte(respBodyString), &respDto)
-			assert.Nil(_t, jsonUnmarshallingError)
+// 			//	-- convert to json and check for error
+// 			var respDto dtos.VariantReponse
+// 			jsonUnmarshallingError := json.Unmarshal([]byte(respBodyString), &respDto)
+// 			assert.Nil(_t, jsonUnmarshallingError)
 
-		}(vcfgz, &combWg)
-	}
+// 		}(vcfgz, &combWg)
+// 	}
 
-	// allowing all lines to be queued up and waited for
-	for i := 0; i < simultaneousRequestsCapacity; i++ {
-		simultaneousRequestsQueue <- true
-	}
+// 	// allowing all lines to be queued up and waited for
+// 	for i := 0; i < simultaneousRequestsCapacity; i++ {
+// 		simultaneousRequestsQueue <- true
+// 	}
 
-	combWg.Wait()
-}
+// 	combWg.Wait()
+// }
