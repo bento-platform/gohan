5e19c5a526e6739129b2a223711307dd85afe06b
David Lougheed <david.lougheed@gmail.com>
2023-03-15 16:26:07 -0400

    switch back to BENTO_UID for consistency

--- elasticsearch/gohan_create_service_user.bash
+++ elasticsearch/gohan_create_service_user.bash
@@ -2,7 +2,7 @@
 
 # If set, use the local UID from outside the container (or default to 1001; 1000 is already created by the
 # Elasticsearch container)
-USER_ID=${GOHAN_UID:-1001}
+USER_ID=${BENTO_UID:-1001}
 
 echo "[gohan_elasticsearch] [/gohan_create_service_user.bash] using USER_ID=${USER_ID}"
 
