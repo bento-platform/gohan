e6e7034f423ce1c742f50c5727adc5b80cdf2828
David Lougheed <david.lougheed@gmail.com>
2023-03-15 10:30:36 -0400

    feat: permissions-fixed, updated elasticsearch image ci: update BBA for ES to 0.11

---
+++ elasticsearch/gohan_entrypoint.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /gohan_create_service_user.bash
+
+# Fix permissions on Elasticsearch directories
+# See https://www.elastic.co/guide/en/elasticsearch/reference/7.17/docker.html#_configuration_files_must_be_readable_by_the_elasticsearch_user
+#  - except we use a different user!
+chown gohan_user:gohan_user /usr/share/elasticsearch/config
+chown gohan_user:gohan_user /usr/share/elasticsearch/data
+chown gohan_user:gohan_user /usr/share/elasticsearch/logs
+
+# Drop into gohan_user from root and execute the CMD specified for the image
+exec gosu gohan_user "$@"
