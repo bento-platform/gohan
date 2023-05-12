f47a8d2a09d4f824c9b336eb4e18003957c9ed5d
Brennan Brouillette <brennan.brouillette@computationalgenomics.ca>
2023-05-12 01:39:30 -0400

    chore: added permission verbs

--- src/api/models/constants/authorization/main.go
+++ src/api/models/constants/authorization/main.go
@@ -3,7 +3,15 @@ package authorization
 import "gohan/api/models/constants"
 
 const (
-	QUERY constants.PermissionVerb = "query"
+	QUERY    constants.PermissionVerb = "query"
+	DOWNLOAD constants.PermissionVerb = "download"
+	VIEW     constants.PermissionVerb = "view"
+	CREATE   constants.PermissionVerb = "create"
+	EDIT     constants.PermissionVerb = "edit"
+	DELEVE   constants.PermissionVerb = "delete"
+	INGEST   constants.PermissionVerb = "ingest"
+	ANALYZE  constants.PermissionVerb = "analyze"
+	EXPORT   constants.PermissionVerb = "export"
 )
 
 const (
