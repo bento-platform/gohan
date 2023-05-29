3c1cc725da3e40d201452e8d2a9bb8cc5feae8d0
Brennan Brouillette <brennan.brouillette@computationalgenomics.ca>
2023-05-29 14:06:12 -0400

    patch: add fields to data-type response - "queryable" and "count"

--- src/api/mvc/data-types/main.go
+++ src/api/mvc/data-types/main.go
@@ -3,20 +3,32 @@ package dataTypes
 import (
 	"net/http"
 
+	"gohan/api/contexts"
 	"gohan/api/models/schemas"
+
+	variantService "gohan/api/services/variants"
 
 	"github.com/labstack/echo"
 )
 
 var variantDataTypeJson = map[string]interface{}{
-	"id":     "variant",
-	"label":  "Variants",
-	"schema": schemas.VARIANT_SCHEMA,
+	"id":        "variant",
+	"label":     "Variants",
+	"queryable": true,
+	"schema":    schemas.VARIANT_SCHEMA,
 }
 
 // "metadata_schema": schemas.VARIANT_TABLE_METADATA_SCHEMA,
-
 func GetDataTypes(c echo.Context) error {
+	es := c.(*contexts.GohanContext).Es7Client
+	cfg := c.(*contexts.GohanContext).Config
+
+	// accumulate number of variants associated with each
+	// sampleId fetched from the variants overview
+	// TODO: refactor to handle errors better
+	resultsMap := variantService.GetVariantsOverview(es, cfg)
+	variantDataTypeJson["count"] = sumAllValues(resultsMap["sampleIDs"])
+
 	// Data types are basically stand-ins for schema blocks
 	return c.JSON(http.StatusOK, []map[string]interface{}{
 		variantDataTypeJson,
@@ -33,4 +45,14 @@ func GetVariantDataTypeSchema(c echo.Context) error {
 
 func GetVariantDataTypeMetadataSchema(c echo.Context) error {
 	return c.JSON(http.StatusOK, schemas.VARIANT_TABLE_METADATA_SCHEMA)
+}
+
+// - helpers
+func sumAllValues(keyedValues interface{}) float64 {
+	tmpValueStrings := keyedValues.(map[string]interface{})
+	sum := 0.0
+	for _, k := range tmpValueStrings {
+		sum += k.(float64)
+	}
+	return sum
 }
