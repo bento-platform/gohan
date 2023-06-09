fe07480596c79bd984779e721ca436c464ec3cbe
Brennan Brouillette <brennan.brouillette@computationalgenomics.ca>
2023-06-09 17:10:45 -0400

    patch: redundant function

--- src/api/middleware/assemblyMiddleware.go
+++ src/api/middleware/assemblyMiddleware.go
@@ -28,28 +28,3 @@ func MandateAssemblyIdAttribute(next echo.HandlerFunc) echo.HandlerFunc {
 		return next(gc)
 	}
 }
-
-/*
-Echo middleware to ensure the optional `assemblyId`
-HTTP query parameter is valid if present
-*/
-func OptionalAssemblyIdAttribute(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		gc := c.(*contexts.GohanContext)
-
-		// check for assemblyId query parameter
-		assemblyId := c.QueryParam("assemblyId")
-		if len(assemblyId) > 0 {
-			if !assid.IsKnownAssemblyId(assemblyId) {
-				// if an id was provided and was invalid, return an error
-				return echo.NewHTTPError(http.StatusBadRequest, "Invalid assemblyId!")
-			}
-			// forward a type-safe value down the pipeline
-			gc.AssemblyId = constants.AssemblyId(assemblyId)
-		} else {
-			gc.AssemblyId = assid.Unknown
-		}
-
-		return next(gc)
-	}
-}
