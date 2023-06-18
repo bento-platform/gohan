106b220a1e09b65cc83575aa0361f5aabe776c41
Brennan Brouillette <brennan.brouillette@computationalgenomics.ca>
2023-06-18 01:49:00 -0400

    chore: refactoring build tests, common tooling

--- src/api/tests/build/api/variants_test.go
+++ src/api/tests/build/api/variants_test.go
@@ -29,44 +29,30 @@ func TestIngest(t *testing.T) {
 	assert.True(t, t.Run("Ingest Demo VCF", func(t *testing.T) {
 		// create demo vcf string
 		sampleId := "abc1234"
-		vcfString := `##fileformat=VCFv4.2
-#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	S-1178-HAP
-1	13656	.	CAG	C,<NON_REF>	868.60	.	BaseQRankSum=-5.505;DP=81;ExcessHet=3.0103;MLEAC=1,0;MLEAF=0.500,0.00;MQRankSum=-2.985;RAW_MQandDP=43993,81;ReadPosRankSum=-0.136	GT:AD:DP:GQ:PL:SB	0:50,25,0:75:99:876,0,2024,1026,2099,3126:4,46,5,20
-10	28872481	.	CAAAA	C,CA,CAAA,CAAAAA,CAAAAAA,<NON_REF>	652.60	.	BaseQRankSum=0.029;DP=83;ExcessHet=3.0103;MLEAC=0,0,0,1,0,0;MLEAF=0.00,0.00,0.00,0.500,0.00,0.00;MQRankSum=-0.186;RAW_MQandDP=291409,83;ReadPosRankSum=-0.582	GT:AD:DP:GQ:PL:SB	0:19,3,2,5,29,9,0:67:99:660,739,2827,748,2714,2732,724,1672,1682,1587,0,340,338,249,265,321,956,929,699,245,898,866,1996,1991,1652,466,1006,1944:0,19,0,48
-19	3619025	.	C	<NON_REF>	.	.	END=3619025	GT:DP:GQ:MIN_DP:PL	0:19:21:19:0,21,660
-19	3619026	.	T	<NON_REF>	.	.	END=3619026	GT:DP:GQ:MIN_DP:PL	0:19:51:19:0,51,765`
 
 		// - save string to vcf directory
-		localDataRootPath := getRootGohanPath() + "/data"
+		localDataRootPath := common.GetRootGohanPath() + "/data"
 		localVcfPath := localDataRootPath + "/vcfs"
 
 		newFilePath := fmt.Sprintf("%s/%s.vcf", localVcfPath, sampleId)
 
 		// - create file if not exists
-		var (
-			file *os.File
-			err  error
-		)
-
-		file, err = os.Create(newFilePath)
-		if isError(err) {
-			return
+		file, err := common.CreateAndGetNewFile(newFilePath)
+		if err != nil {
+			log.Fatal(err)
 		}
-		defer file.Close()
-
-		// - reopen file using READ & WRITE permission.
-		file, err = os.OpenFile(newFilePath, os.O_RDWR, 0644)
-		if isError(err) {
-			return
-		}
-		defer file.Close()
+		defer func() {
+			file.Close()
+		}()
 
 		// - write some vcf string to file.
-		_, err = file.WriteString(vcfString)
-		if isError(err) {
+		_, err = file.WriteString(common.DemoVcf1)
+		if common.IsError(err) {
 			return
 		}
-		defer func() { os.Remove(newFilePath) }()
+		defer func() {
+			os.Remove(newFilePath)
+		}()
 
 		// compress the vcf file with bgzip
 		out, err := exec.Command("bgzip", newFilePath).Output()
@@ -77,7 +63,9 @@ func TestIngest(t *testing.T) {
 		fmt.Println(string(out))
 
 		newGzFile := newFilePath + ".gz"
-		defer func() { os.Remove(newGzFile) }()
+		defer func() {
+			os.Remove(newGzFile)
+		}()
 
 		// - ingest
 		assemblyId := "GRCh38"
@@ -90,6 +78,7 @@ func TestIngest(t *testing.T) {
 		assert.True(t, len(initialIngestionDtos) > 0)
 
 		// check ingestion request
+		// TODO: avoid potential infinite loop
 		for {
 			fmt.Println("Checking state of the ingestion..")
 
@@ -118,6 +107,7 @@ func TestIngest(t *testing.T) {
 		}
 
 		// check ingestion stats
+		// TODO: avoid potential infinite loop
 		for {
 			fmt.Println("Checking ingestion stats..")
 			// pause
@@ -152,28 +142,4 @@ func TestIngest(t *testing.T) {
 		dtos := common.BuildQueryAndMakeGetVariantsCall("1", "*", true, "asc", "", "GRCh38", "", "", "", false, t, cfg)
 		assert.True(t, len(dtos.Results[0].Calls) > 0)
 	}))
-}
-
-func isError(err error) bool {
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	return (err != nil)
-}
-
-func getRootGohanPath() string {
-	// check if file exists
-	wd, err1 := os.Getwd()
-	if err1 != nil {
-		log.Println(err1)
-	}
-	fmt.Println(wd) // for example /home/user
-
-	path := filepath.Dir(wd)
-	for i := 1; i < 5; i++ {
-		path = filepath.Dir(path)
-	}
-
-	return path
 }
--- src/api/tests/common/common.go
+++ src/api/tests/common/common.go
@@ -9,9 +9,11 @@ import (
 	gq "gohan/api/models/constants/genotype-query"
 	"gohan/api/models/dtos"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 	"runtime"
 	"testing"
 
@@ -23,6 +25,13 @@ const (
 	VariantsOverviewPath                      string = "%s/variants/overview"
 	VariantsGetBySampleIdsPathWithQueryString string = "%s/variants/get/by/sampleId%s"
 	IngestionRequestsPath                     string = "%s/variants/ingestion/requests"
+
+	DemoVcf1 string = `##fileformat=VCFv4.2
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	S-1178-HAP
+1	13656	.	CAG	C,<NON_REF>	868.60	.	BaseQRankSum=-5.505;DP=81;ExcessHet=3.0103;MLEAC=1,0;MLEAF=0.500,0.00;MQRankSum=-2.985;RAW_MQandDP=43993,81;ReadPosRankSum=-0.136	GT:AD:DP:GQ:PL:SB	0:50,25,0:75:99:876,0,2024,1026,2099,3126:4,46,5,20
+10	28872481	.	CAAAA	C,CA,CAAA,CAAAAA,CAAAAAA,<NON_REF>	652.60	.	BaseQRankSum=0.029;DP=83;ExcessHet=3.0103;MLEAC=0,0,0,1,0,0;MLEAF=0.00,0.00,0.00,0.500,0.00,0.00;MQRankSum=-0.186;RAW_MQandDP=291409,83;ReadPosRankSum=-0.582	GT:AD:DP:GQ:PL:SB	0:19,3,2,5,29,9,0:67:99:660,739,2827,748,2714,2732,724,1672,1682,1587,0,340,338,249,265,321,956,929,699,245,898,866,1996,1991,1652,466,1006,1944:0,19,0,48
+19	3619025	.	C	<NON_REF>	.	.	END=3619025	GT:DP:GQ:MIN_DP:PL	0:19:21:19:0,21,660
+19	3619026	.	T	<NON_REF>	.	.	END=3619026	GT:DP:GQ:MIN_DP:PL	0:19:51:19:0,51,765`
 )
 
 func InitConfig() *models.Config {
@@ -96,6 +105,50 @@ func GetVariantsOverview(_t *testing.T, _cfg *models.Config) map[string]interfac
 	assert.NotNil(_t, sampleIDsKey)
 
 	return overviewRespJson
+}
+
+func GetRootGohanPath() string {
+	// check if file exists
+	wd, err1 := os.Getwd()
+	if err1 != nil {
+		log.Println(err1)
+	}
+	fmt.Println(wd) // for example /home/user
+
+	path := filepath.Dir(wd)
+	for i := 1; i < 5; i++ {
+		path = filepath.Dir(path)
+	}
+
+	return path
+}
+
+func IsError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return (err != nil)
+}
+
+func CreateAndGetNewFile(filePath string) (*os.File, error) {
+	// - create file if not exists
+	var (
+		newFile    *os.File
+		newFileErr error
+	)
+
+	_, newFileErr = os.Create(filePath)
+	if IsError(newFileErr) {
+		return nil, newFileErr
+	}
+
+	// - reopen file using READ & WRITE permission.
+	newFile, newFileErr = os.OpenFile(filePath, os.O_RDWR, 0644)
+	if IsError(newFileErr) {
+		return nil, newFileErr
+	}
+	return newFile, newFileErr
 }
 
 func BuildQueryAndMakeGetVariantsCall(
