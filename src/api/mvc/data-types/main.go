a9d701bf5bd8fee585ba108c895e169a1f8416af
Julian <ornatus30@gmail.com>
2023-10-10 13:56:45 -0400

    Add function to retrieve reduced dataTypes

--- src/api/mvc/data-types/main.go
+++ src/api/mvc/data-types/main.go
@@ -51,9 +51,37 @@ func GetDataTypes(c echo.Context) error {
 	})
 }
 
-	// Data types are basically stand-ins for schema blocks
+func GetReducedDataTypes(c echo.Context) error {
+	variantData, err := fetchVariantData(c)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"error": err.Error(),
+		})
+	}
+
+	if variantData == nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"error": "Failed to retrieve variant data.",
+		})
+	}
+
+	count, _ := variantData["count"]
+	id, _ := variantData["id"].(string)
+	label, _ := variantData["label"].(string)
+	last_ingested, _ := variantData["last_ingested"].(string)
+	queryable, _ := variantData["queryable"].(bool)
+
+	// Create a reduced response
+	reducedResponse := map[string]interface{}{
+		"count":         count,
+		"id":            id,
+		"label":         label,
+		"last_ingested": last_ingested,
+		"queryable":     queryable,
+	}
+
 	return c.JSON(http.StatusOK, []map[string]interface{}{
-		variantDataTypeJson,
+		reducedResponse,
 	})
 }
 
