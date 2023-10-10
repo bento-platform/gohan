1898fbae7d855c9cd96b0ff95ad4d62096e22f89
Julian <ornatus30@gmail.com>
2023-10-10 13:56:05 -0400

    Refactor GetDataTypes

--- src/api/mvc/data-types/main.go
+++ src/api/mvc/data-types/main.go
@@ -20,30 +20,36 @@ var variantDataTypeJson = map[string]interface{}{
 	"metadata_schema": schemas.OBJECT_SCHEMA,
 }
 
-func GetDataTypes(c echo.Context) error {
+func fetchVariantData(c echo.Context) (map[string]interface{}, error) {
 	gc := c.(*contexts.GohanContext)
 	cfg := gc.Config
 	es := gc.Es7Client
 
-	// accumulate number of variants associated with each
-	// sampleId fetched from the variants overview
 	resultsMap, err := variantService.GetVariantsOverview(es, cfg)
-
 	if err != nil {
-		// Could not talk to Elasticsearch, return an error
+		return nil, err
+	}
+
+	variantDataTypeJson["count"] = sumAllValues(resultsMap["sampleIDs"])
+	if latestCreated, ok := resultsMap["last_created_time"].(string); ok {
+		variantDataTypeJson["last_ingested"] = latestCreated
+	}
+
+	return variantDataTypeJson, nil
+}
+
+func GetDataTypes(c echo.Context) error {
+	variantData, err := fetchVariantData(c)
+	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
 			"error": err.Error(),
 		})
 	}
 
-	variantDataTypeJson["count"] = sumAllValues(resultsMap["sampleIDs"])
-
-	// Fetch the last_created value from resultsMap and add to variantDataTypeJson
-	if latestCreated, ok := resultsMap["last_created_time"].(string); ok {
-		variantDataTypeJson["last_created"] = latestCreated
-	}
-
-	fmt.Printf("variantDataTypeJson: %v\n", variantDataTypeJson)
+	return c.JSON(http.StatusOK, []map[string]interface{}{
+		variantData,
+	})
+}
 
 	// Data types are basically stand-ins for schema blocks
 	return c.JSON(http.StatusOK, []map[string]interface{}{
